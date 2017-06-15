99d1db4776620041e8154755130d13fa9f28dcc9
gabrielcs23 <gabrielcs23@hotmail.com>
2017-06-15 18:28:11 -0300

    Create ArvBArq.c

--- ArvBArq.c
+++ ArvBArq.c
@@ -8,16 +8,25 @@ int n_arq = 1;
 void cria (int num, char *nomee)
 {
     int ni  = 1;
-    char nome[25];
-    sprintf(nome,"%d" , n_arq);
-    strcat(nome,".dat");
-    strcpy(nomee,nome);
-    FILE *fp = fopen(nome,"wb");
+    if (n_arq != 0)
+    {
+        char nome[NOME_MAX];
+        sprintf(nome,"%d" , n_arq);
+        strcat(nome,".dat");
+        strcpy(nomee,nome);
+        FILE *fp = fopen(nome,"wb");
+        fwrite(&ni,sizeof(int),1,fp);
+        fwrite(&num,sizeof(int),1,fp);
+        n_arq++;
+        fclose(fp);
+        return;
+    }
+    FILE *fp = fopen("raiz.dat", "wb");
     fwrite(&ni,sizeof(int),1,fp);
     fwrite(&num,sizeof(int),1,fp);
     n_arq++;
     fclose(fp);
-    return;
+    strcpy(nomee,"raiz.dat");
 }
 
 int retorna_filho(char* arq, int filho_n, char* strin)
@@ -27,9 +36,9 @@ int retorna_filho(char* arq, int filho_n, char* strin)
     fread(&num_filhos_total, sizeof(int),1,fp);
     if(filho_n > num_filhos_total) return 0;
     fseek(fp,sizeof(int)*num_filhos_total,SEEK_CUR);
-    fseek(fp,sizeof(char)*25*filho_n,SEEK_CUR);
-    char retorno[25];
-    int leu = fread(retorno, sizeof(char)*25,1,fp);
+    fseek(fp,sizeof(char)*NOME_MAX*filho_n,SEEK_CUR);
+    char retorno[NOME_MAX];
+    int leu = fread(retorno, sizeof(char)*NOME_MAX,1,fp);
     fclose(fp);
     if(!leu) return 0;
     strcpy(strin, retorno);
@@ -37,17 +46,14 @@ int retorna_filho(char* arq, int filho_n, char* strin)
 }
 
 void libera(char *arq){
-    FILE *fp = fopen(arq,"rb+");
-    int nchaves;
-    fread(&nchaves,sizeof(int),1,fp);
-    fseek(fp,nchaves,SEEK_CUR);
-    int no_interno;
-    char nome[NOME_MAX];
-    while(1){
-        no_interno = fread(&nome,sizeof(char),NOME_MAX,fp);
-        if(!no_interno) break;
-        libera(nome);
+    char nome_filho[NOME_MAX];
+    int acao = retorna_filho(arq,0,nome_filho);
+    int ind = 0;
+    while(acao){
+        libera(nome_filho);
+        remove(nome_filho);
+        ind++;
+        acao = retorna_filho(arq,ind,nome_filho);
     }
-    fclose(fp);
     remove(arq);
 }
